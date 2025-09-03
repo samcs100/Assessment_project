5799a0d3c0497d8169b0f4dca9e7013f227c348d
samcs100 <karansourav02@gmail.com>
2025-09-03 20:40:18 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
+
+export default defineConfig({
+	plugins: [
+		Vue(),
+		AutoImport({
+			imports: [
+				'vue',
+			],
+		}),
+	],
+})
